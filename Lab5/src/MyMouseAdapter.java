26ceef666e539981e30e816bde1d25ae4ad61c73
Joian Velez <josian.velez@upr.edu>
2017-03-21 15:09:13 -0400

    implemented recursive step to make the games main things happen . the display of amount of bombs

--- Lab5/src/MyMouseAdapter.java
+++ Lab5/src/MyMouseAdapter.java
@@ -8,70 +8,70 @@ import java.util.Random;
 import javax.swing.JFrame;
 
 public class MyMouseAdapter extends MouseAdapter {
-	private Random generator = new Random();
-	public void mousePressed(MouseEvent e) {
-		switch (e.getButton()) {
-			case 1:		//Left mouse button
-				Component c = e.getComponent();
-				while (!(c instanceof JFrame)) {
-					c = c.getParent();
-					if (c == null) {
-						return;
-					}
-				}
-				JFrame myFrame = (JFrame) c;
-				MyPanel myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);
-				Insets myInsets = myFrame.getInsets();
-				int x1 = myInsets.left;
-				int y1 = myInsets.top;
-				e.translatePoint(-x1, -y1);
-				int x = e.getX();
-				int y = e.getY();
-				myPanel.x = x;
-				myPanel.y = y;
-				myPanel.mouseDownGridX = myPanel.getGridX(x, y);
-				myPanel.mouseDownGridY = myPanel.getGridY(x, y);
-				myPanel.repaint();
-				break;
-			case 3:		//Right mouse button
-				break;
-			default:    //Some other button (2 = Middle mouse button, etc.)
-				break;
-		}
-	}
+    private Random generator = new Random();
+    public void mousePressed(MouseEvent e) {
+        switch (e.getButton()) {
+            case 1:		//Left mouse button
+                Component c = e.getComponent();
+                while (!(c instanceof JFrame)) {
+                    c = c.getParent();
+                    if (c == null) {
+                        return;
+                    }
+                }
+                JFrame myFrame = (JFrame) c;
+                MyPanel myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);
+                Insets myInsets = myFrame.getInsets();
+                int x1 = myInsets.left;
+                int y1 = myInsets.top;
+                e.translatePoint(-x1, -y1);
+                int x = e.getX();
+                int y = e.getY();
+                myPanel.x = x;
+                myPanel.y = y;
+                myPanel.mouseDownGridX = myPanel.getGridX(x, y);
+                myPanel.mouseDownGridY = myPanel.getGridY(x, y);
+                myPanel.repaint();
+                break;
+            case 3:		//Right mouse button
+                break;
+            default:    //Some other button (2 = Middle mouse button, etc.)
+                break;
+        }
+    }
 
-	public void mouseReleased(MouseEvent e) {
-		switch (e.getButton()) {
-			case 1:		//Left mouse button
-				Component c = e.getComponent();
-				while (!(c instanceof JFrame)) {
-					c = c.getParent();
-					if (c == null) {
-						return;
-					}
-				}
-				JFrame myFrame = (JFrame)c;
-				MyPanel myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);  //Can also loop among components to find MyPanel
-				Insets myInsets = myFrame.getInsets();
-				int x1 = myInsets.left;
-				int y1 = myInsets.top;
-				e.translatePoint(-x1, -y1);
-				int x = e.getX();
-				int y = e.getY();
-				myPanel.x = x;
-				myPanel.y = y;
-				int gridX = myPanel.getGridX(x, y);
-				int gridY = myPanel.getGridY(x, y);
-				if ((myPanel.mouseDownGridX == -1) || (myPanel.mouseDownGridY == -1)) {
-					//Had pressed outside originally and now inside
-				} else {
-					if ((gridX == -1) || (gridY == -1)) {
-						//Is releasing outside the grid
-					} else {
-						if ((myPanel.mouseDownGridX != gridX) || (myPanel.mouseDownGridY != gridY)) {
-							//Released the mouse button on a different cell where it was pressed
-						} else {
-							//Released the mouse button on the same cell where it was pressed
+    public void mouseReleased(MouseEvent e) {
+        switch (e.getButton()) {
+            case 1:		//Left mouse button
+                Component c = e.getComponent();
+                while (!(c instanceof JFrame)) {
+                    c = c.getParent();
+                    if (c == null) {
+                        return;
+                    }
+                }
+                JFrame myFrame = (JFrame)c;
+                MyPanel myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);  //Can also loop among components to find MyPanel
+                Insets myInsets = myFrame.getInsets();
+                int x1 = myInsets.left;
+                int y1 = myInsets.top;
+                e.translatePoint(-x1, -y1);
+                int x = e.getX();
+                int y = e.getY();
+                myPanel.x = x;
+                myPanel.y = y;
+                int gridX = myPanel.getGridX(x, y);
+                int gridY = myPanel.getGridY(x, y);
+                if ((myPanel.mouseDownGridX == -1) || (myPanel.mouseDownGridY == -1)) {
+                    //Had pressed outside originally and now inside
+                } else {
+                    if ((gridX == -1) || (gridY == -1)) {
+                        //Is releasing outside the grid
+                    } else {
+                        if ((myPanel.mouseDownGridX != gridX) || (myPanel.mouseDownGridY != gridY)) {
+                            //Released the mouse button on a different cell where it was pressed
+                        } else {
+                            //Released the mouse button on the same cell where it was pressed
                             //and on grid
 
                             ///////////////////////////////////////////////////////////////////////////
@@ -96,70 +96,61 @@ public class MyMouseAdapter extends MouseAdapter {
                             ///////////////////////////////////////////////////////////////////////////
                             ///////////////////////////////////////////////////////////////////////////
                             else{
+
                                 checkSurroundings(myPanel.mouseDownGridX,myPanel.mouseDownGridY,myPanel);
-                                myPanel.repaint();
                             }
                         }
-					}
-				}
-				myPanel.repaint();
-				break;
-			case 3:		//Right mouse button
-				break;
-			default:    //Some other button (2 = Middle mouse button, etc.)
-				break;
-		}
-	}
-
-    private int checkSurroundings(int mouseDownGridX, int mouseDownGridY,MyPanel myPanel) {
-
-	    if(mouseDownGridX!=0 && mouseDownGridY!=0&& mouseDownGridX != 8 && mouseDownGridY!=8){
-	        if(myPanel.bombLocations[myPanel.mouseDownGridX-1][myPanel.mouseDownGridY-1]){
-                myPanel.bombsAroundXY[myPanel.mouseDownGridX][myPanel.mouseDownGridY]=+1;
-            }else {
-                checkSurroundings(myPanel.mouseDownGridX-1,myPanel.mouseDownGridY-1,myPanel);
-
-            }if(myPanel.bombLocations[myPanel.mouseDownGridX][myPanel.mouseDownGridY-1]){
-                myPanel.bombsAroundXY[myPanel.mouseDownGridX][myPanel.mouseDownGridY]=+1;
-            }else {
-                checkSurroundings(myPanel.mouseDownGridX,myPanel.mouseDownGridY-1,myPanel);
-
-            }if(myPanel.bombLocations[myPanel.mouseDownGridX+1][myPanel.mouseDownGridY-1]){
-                myPanel.bombsAroundXY[myPanel.mouseDownGridX][myPanel.mouseDownGridY]=+1;
-            }else {
-                checkSurroundings(myPanel.mouseDownGridX+1,myPanel.mouseDownGridY-1,myPanel);
-
-            }if(myPanel.bombLocations[myPanel.mouseDownGridX-1][myPanel.mouseDownGridY]){
-                myPanel.bombsAroundXY[myPanel.mouseDownGridX][myPanel.mouseDownGridY]=+1;
-            }else {
-                checkSurroundings(myPanel.mouseDownGridX-1,myPanel.mouseDownGridY,myPanel);
-
-            }if(myPanel.bombLocations[myPanel.mouseDownGridX+1][myPanel.mouseDownGridY]){
-                myPanel.bombsAroundXY[myPanel.mouseDownGridX][myPanel.mouseDownGridY]=+1;
-            }else {
-                checkSurroundings(myPanel.mouseDownGridX+1,myPanel.mouseDownGridY,myPanel);
-
-            }if(myPanel.bombLocations[myPanel.mouseDownGridX-1][myPanel.mouseDownGridY+1]){
-                myPanel.bombsAroundXY[myPanel.mouseDownGridX][myPanel.mouseDownGridY]=+1;
-            }else {
-                checkSurroundings(myPanel.mouseDownGridX-1,myPanel.mouseDownGridY+1,myPanel);
-
-            }if(myPanel.bombLocations[myPanel.mouseDownGridX][myPanel.mouseDownGridY+1]){
-                myPanel.bombsAroundXY[myPanel.mouseDownGridX][myPanel.mouseDownGridY]=+1;
-            }else {
-                checkSurroundings(myPanel.mouseDownGridX,myPanel.mouseDownGridY+1,myPanel);
-
-            }if(myPanel.bombLocations[myPanel.mouseDownGridX+1][myPanel.mouseDownGridY+1]){
-                myPanel.bombsAroundXY[myPanel.mouseDownGridX][myPanel.mouseDownGridY]=+1;
-            }else {
-                checkSurroundings(myPanel.mouseDownGridX+1,myPanel.mouseDownGridY+1,myPanel);
-
-            }
-
-        }else{
-	        System.out.println("Border");
+                    }
+                }
+                myPanel.repaint();
+                break;
+            case 3:		//Right mouse button
+                break;
+            default:    //Some other button (2 = Middle mouse button, etc.)
+                break;
         }
-        return myPanel.bombsAroundXY[myPanel.mouseDownGridX][myPanel.mouseDownGridY];
+    }
+
+    private void checkSurroundings(int mouseDownGridX, int mouseDownGridY, MyPanel myPanel) {
+
+
+        if(myPanel.bombsAroundXY[mouseDownGridX][mouseDownGridY]==-1) {
+            myPanel.bombsAroundXY[mouseDownGridX][mouseDownGridY]=0;
+
+            for (int x = mouseDownGridX - 1; x <= mouseDownGridX + 1; x++) {
+                if((x<0 ||x>myPanel.TOTAL_COLUMNS-1)){
+                    continue;
+                }
+                for (int y = mouseDownGridY - 1; y <= mouseDownGridY + 1; y++) {
+                    if((y<0 || y>myPanel.TOTAL_ROWS-2)){
+                        continue;
+                    }
+                    if(x == mouseDownGridX && y == mouseDownGridY){
+                        continue;
+                    }
+                    if (myPanel.bombLocations[x][y]) {
+                        myPanel.bombsAroundXY[mouseDownGridX][mouseDownGridY]++;
+                    }
+                }
+            }
+            if(myPanel.bombsAroundXY[mouseDownGridX][mouseDownGridY]==0){
+                for (int x = mouseDownGridX - 1; x <= mouseDownGridX + 1; x++) {
+                    if ((x < 0 || x > myPanel.TOTAL_COLUMNS - 1)) {
+                        continue;
+                    }
+                    for (int y = mouseDownGridY - 1; y <= mouseDownGridY + 1; y++) {
+                        if ((y < 0 || y > myPanel.TOTAL_ROWS - 2)) {
+                            continue;
+                        }
+                        checkSurroundings(x,y,myPanel);
+
+
+
+                    }
+                }
+            }
+            myPanel.repaint();
+        }
     }
 
 
